83b7713979474e158b9f23461bbf739d904fcfd4
Attar <attaralwie@gmail.com>
2024-11-18 03:52:43 +0700

    Finish main app

--- main/views.py
+++ main/views.py
@@ -7,3 +7,11 @@ from django.contrib.auth.decorators import login_required
 def show_main(request):
     context = {"user": request.user}   
     return render(request, "homepage.html", context)
+
+@login_required(login_url='/auth')
+def show_subkategori(request):
+    context = {"user": request.user}   
+    if request.user.role == "pengguna" :
+        return render(request, "subkategori_pengguna.html", context)
+    else :
+        return render(request, "subkategori_pekerja.html", context)

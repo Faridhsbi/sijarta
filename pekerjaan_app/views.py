df57f4b469615a3eb573afb876931e3766c3c2a6
Edmond Christian <edmond.chr@gmail.com>
2024-12-08 14:48:54 +0700

    removed placeholder

--- pekerjaan_app/views.py
+++ pekerjaan_app/views.py
@@ -196,31 +196,7 @@ def handle_kerjakan_pesanan(request):
 
     return redirect('pekerjaan_app:show_pekerjaan')
 
-# def show_status_pekerjaan(request): # PLACEHOLDER
-#     user = request.user
-#     if user.role != 'pekerja':
-#         messages.error(request, "Anda tidak memiliki izin untuk mengakses halaman ini.")
-#         return redirect('main:show_main')
-    
 
-#     kategori1 = {'kategori': 'Home Cleaning', 'subkategori': ['Setrika', 'Daily Cleaning', 'Pembersihan Dapur']}
-#     kategori2 = {'kategori': 'Massage', 'subkategori': ['Foot massage', 'Back massage', 'Arm massage', 'Full package']}
-#     kategori3 = {'kategori': 'Deep Cleaning', 'subkategori': ['Cuci kasur', 'Cuci sofa']}
-
-#     pekerjaan1 = {'id': 1, 'kategori': 'Home Cleaning', 'subkategori': 'Setrika', 'nama_pelanggan': 'Budi', 'tanggal_pemesanan': '10-11-2024', 'tanggal_pekerjaan': '20-11-2024', 'total_biaya': 150000, 'status': 'Menunggu Pekerja Berangkat'}
-#     pekerjaan2 = {'id': 2, 'kategori': 'Home Cleaning', 'subkategori': 'Setrika', 'nama_pelanggan': 'Caca', 'tanggal_pemesanan': '15-11-2024', 'tanggal_pekerjaan': '22-11-2024', 'total_biaya': 100000, 'status': 'Pekerja Tiba Di Lokasi'}
-
-#     pekerjaan5 = {'id': 5, 'kategori': 'Massage', 'subkategori': 'Foot massage', 'nama_pelanggan': 'asdsadv', 'tanggal_pemesanan': '15-11-2024', 'tanggal_pekerjaan': '16-11-2024', 'total_biaya': 130250, 'status': 'Pelayanan Jasa Sedang Dilakukan'}
-#     pekerjaan7 = {'id': 7, 'kategori': 'Massage', 'subkategori': 'Arm massage', 'nama_pelanggan': 'efea', 'tanggal_pemesanan': '19-11-2024', 'tanggal_pekerjaan': '20-11-2024', 'total_biaya': 300000, 'status': 'Pesanan Selesai'}
-    
-#     context = {
-#         'no_hp' : '0857111',
-#         'saldo_mypay' : 200000,
-#         'kategori' : [kategori1, kategori2, kategori3],
-#         'pekerjaan' : [pekerjaan1, pekerjaan2, pekerjaan5, pekerjaan7],
-#     }
-
-#     return render(request, 'status_pekerjaan.html', context)
 
 def show_status_pekerjaan(request):
     user_id = get_cookie(request, 'user_id')
